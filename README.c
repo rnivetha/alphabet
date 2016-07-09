96d06527595e7296158d6f30e950d338dec61077
rnivetha <rnivetha1995@gmail.com>
2016-07-08 23:05:42 -0700

    Update and rename README.md to README.c

---
+++ README.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    char c;
+    printf("Enter a character");
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf("it is an alphabet");
+    else
+        printf("it is not an alphabet");
+
+}
